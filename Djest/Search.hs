7f7ac92cdad47a86376d16254e5d57f0801c7656
Luke Palmer <lrpalmer@gmail.com>
2018-01-06 14:06:36 -0800

    Implemented hints

--- Djest/Search.hs
+++ Djest/Search.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, RankNTypes, ScopedTypeVariables, TemplateHaskell #-}
+{-# LANGUAGE DataKinds, LambdaCase, RankNTypes, ScopedTypeVariables, TemplateHaskell #-}
 
 module Djest.Search (search, deduce) where
 
@@ -10,15 +10,26 @@ import qualified Language.Haskell.TH as TH
 import qualified Language.Haskell.TH.Syntax as TH
 import qualified Data.Map as Map
 import Control.Applicative (liftA2)
+import Control.Monad ((>=>))
+import Data.Maybe (catMaybes)
 import Debug.Trace (trace)
-import Unsafe.Coerce (unsafeCoerce)
 import GHC.Types (Any)
 
-deduce :: String -> TH.Q TH.Type -> TH.Q TH.Exp -> TH.Q [TH.Dec]
-deduce namestr qtype qtests = do
+filterMaybeM :: (Monad m) => (a -> m (Maybe b)) -> [a] -> m [b]
+filterMaybeM p = fmap catMaybes . traverse p
+
+deduce :: String -> TH.Q TH.Type -> [TH.Name] -> TH.Q TH.Exp -> TH.Q [TH.Dec]
+deduce namestr qtype hints qtests = do
+    hintTypes <- filterMaybeM (TH.reify >=> \case 
+                                 TH.VarI n typ _ -> return (Just (n, typ))
+                                 _               -> return Nothing)
+                              hints
+
     let name = TH.mkName namestr
     typ <- qtype
-    t <- decodeType typ
+    let gtyp = foldr (\x y -> TH.AppT (TH.AppT TH.ArrowT x) y) typ (map snd hintTypes)
+
+    t <- decodeType gtyp
 
     typeName <- TH.newName $ "TypeOf_" ++ namestr
     conName  <- TH.newName $ "TypeOf_" ++ namestr
@@ -26,23 +37,26 @@ deduce namestr qtype qtests = do
 
     let typedecs = [
             TH.NewtypeD [] typeName [] Nothing 
-                (TH.RecC conName [(accName, TH.Bang TH.NoSourceUnpackedness TH.NoSourceStrictness, typ)]) [] ]
+                (TH.RecC conName [(accName, TH.Bang TH.NoSourceUnpackedness TH.NoSourceStrictness, gtyp)]) [] ]
 
     testsName <- TH.newName "tests"
-    testsParamName <- TH.newName $ "param_" ++ namestr
+    testsParamName <- TH.newName namestr
     testsBody <- qtests
     Just boolname <- TH.lookupTypeName "Bool"
+
+    let applyHints exp = foldl TH.AppE exp (map (TH.VarE . fst) hintTypes)
+
     let testsDecs =
             [ TH.SigD testsName (TH.AppT (TH.AppT TH.ArrowT (TH.ConT typeName)) (TH.ConT boolname))
             , TH.FunD testsName [TH.Clause [TH.VarP testsParamName] (TH.NormalB testsBody) 
                 [ TH.SigD name typ
                 , TH.FunD name [
-                    TH.Clause [] (TH.NormalB (TH.AppE (TH.VarE accName) (TH.VarE testsParamName))) []
+                    TH.Clause [] (TH.NormalB (applyHints (TH.AppE (TH.VarE accName) (TH.VarE testsParamName)))) []
                     ]
                 ]]
             ]
 
-    insertExp <- [| $(return (TH.VarE accName)) (searchT $(TH.liftData t) $(return (TH.VarE testsName))) |]
+    insertExp <- applyHints <$> [| $(return (TH.VarE accName)) (searchT $(TH.liftData t) $(return (TH.VarE testsName))) |]
     return $ typedecs ++ [TH.SigD name typ, TH.FunD name [TH.Clause [] (TH.NormalB insertExp) testsDecs]]
 
 decodeType :: TH.Type -> TH.Q S.Type
@@ -53,8 +67,8 @@ decodeType = go Map.empty
         | otherwise     = go env body
     go env (TH.ForallT (TH.PlainTV v:vars) cx body) =
         S.TForAll <$> go (Map.insert v 0 (succ <$> env)) (TH.ForallT vars cx body)
-    go env (TH.ForallT (v:vars) cx body) =
-        fail $ "Unsupported binding style " ++ show v
+    go env (TH.ForallT (TH.KindedTV v _:vars) cx body) =
+        S.TForAll <$> go (Map.insert v 0 (succ <$> env)) (TH.ForallT vars cx body)
 
     go env (TH.AppT (TH.AppT TH.ArrowT t) t') = liftA2 (S.:->) (go env t) (go env t')
 
--- thtest.hs
+++ thtest.hs
@@ -1,8 +1,10 @@
-{-# LANGUAGE TemplateHaskell, RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell, RankNTypes, ScopedTypeVariables, KindSignatures #-}
 
 import Djest.Search
 
 deduce "foo" [t| forall a. a -> a -> a |] 
+    ['const]
     [| and [ foo 1 2 == 1
            , foo "x" "y" == "x"
            ] |]
+
