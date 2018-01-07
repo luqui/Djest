32d98541be26cafe3cc6929fce78aec95f0d9b25
Luke Palmer <lrpalmer@gmail.com>
2018-01-07 10:52:54 -0800

    Rewrote church examples to use template haskell

--- examples/church.hs
+++ examples/church.hs
@@ -1,109 +1,62 @@
-{-# LANGUAGE RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes, ScopedTypeVariables, TemplateHaskell, KindSignatures #-}
 
 import Djest.Search
 
-newtype Church = Church { getChurch :: forall r. (r -> r) -> r -> r }
+iter :: Integer -> (a -> a) -> (a -> a)
+iter 0 _ = id
+iter n f = f . iter (n-1) f
 
-toChurch :: Integer -> Church
-toChurch 0 = Church $ \f x -> x
-toChurch n = Church $ \f x -> f (getChurch (toChurch (n-1)) f x)
+zero :: Integer
+zero = 0
 
-fromChurch :: Church -> Integer
-fromChurch ch = getChurch ch succ (0 :: Integer)
+suc :: Integer -> Integer
+suc = succ
 
-addChurch :: Integer -> Integer -> Integer
-addChurch = adapt f
-    where
-    f = search "(forall r. (r -> r) -> r -> r) -> (forall r. (r -> r) -> r -> r) -> (forall r. (r -> r) -> r -> r)" $
-                \(f :: Church -> Church -> Church) ->
-                    let f' = adapt f in
-                    and [
-                        f' 0 0 == 0,
-                        f' 0 1 == 1,
-                        f' 1 0 == 1,
-                        f' 0 2 == 2,
-                        f' 2 0 == 2,
-                        f' 1 1 == 2
-                    ]
-    adapt :: (Church -> Church -> Church) -> Integer -> Integer -> Integer
-    adapt f x y = fromChurch (f (toChurch x) (toChurch y))
+deduce "addChurch" [t| Integer -> Integer -> Integer |]
+    ['iter, 'suc, 'zero]
+    [| and [ addChurch 0 0 == 0
+           , addChurch 0 1 == 1
+           , addChurch 1 0 == 1
+           , addChurch 0 2 == 2
+           , addChurch 2 0 == 2
+           , addChurch 1 1 == 2
+           ] |]
 
-predChurch :: Integer -> Integer
-predChurch = adapt f
-    where
-    f = search "(forall r. (r -> r) -> r -> r) -> (forall r. (r -> r) -> r -> r)" $
-                \(f :: Church -> Church) ->
-                    let f' = adapt f in
-                    and [
-                        f' 1 == 0,
-                        f' 2 == 1,
-                        f' 3 == 2
-                    ]
-    adapt :: (Church -> Church) -> Integer -> Integer
-    adapt f x = fromChurch (f (toChurch x))
+deduce "predChurch" [t| Integer -> Integer |]
+    ['iter, 'suc, 'zero]
+    [| and [ predChurch 1 == 0
+           , predChurch 2 == 1
+           , predChurch 3 == 2
+           ] |]
 
+-- We use a wrapper around List because I'm too lazy to properly
+-- handle ListT in template haskell. :-P
+newtype List a = List { getList :: [a] }
+    deriving (Eq, Show)
 
--- It can do reverse too, though it takes a lot of work to convince the
--- typechecker that we're legit.
+foldList :: (a -> b -> b) -> b -> List a -> b
+foldList c n (List xs) = foldr c n xs
 
-newtype ChurchList a = ChurchList { getChurchList :: forall r. (a -> r -> r) -> r -> r }
+cons :: a -> List a -> List a
+cons x (List xs) = List (x:xs)
 
-fromChurchList :: ChurchList a -> [a]
-fromChurchList l = getChurchList l (:) []
-
-toChurchList :: [a] -> ChurchList a
-toChurchList xs = ChurchList (\f z -> foldr f z xs)
-
-newtype ChurchListFunc = ChurchListFunc { getChurchListFunc :: forall a. ChurchList a -> ChurchList a }
-
-newtype ChurchListSnocFunc = ChurchListSnocFunc { getChurchListSnocFunc :: forall a. (ChurchList a -> a -> ChurchList a) -> ChurchList a -> ChurchList a }
-
--- Deducing reverse is too hard to be done quickly, but if snoc is assumed
--- then it is found immediately.
-reverseChurch :: [a] -> [a]
-reverseChurch = adapt f
-  where
-    f :: ChurchListSnocFunc
-    f = search "forall a. ((forall r. (a -> r -> r) -> r -> r) -> a -> (forall r. (a -> r -> r) -> r -> r)) -> (forall r. (a -> r -> r) -> r -> r) -> (forall r. (a -> r -> r) -> r -> r)" $
-               \(f :: ChurchListSnocFunc) ->
-                  let f' = adapt f in
-                  and [
-                      f' [] == ([] :: [()]),
-                      f' [1,2,3] == [3,2,1]
-                  ]
-    adapt :: ChurchListSnocFunc -> [a] -> [a]
-    adapt f x = fromChurchList (getChurchListSnocFunc f snoc (toChurchList x))
-
-snoc :: ChurchList a -> a -> ChurchList a
-snoc xs x = toChurchList (fromChurchList xs ++ [x])
-
-palinChurch :: [a] -> [a]
-palinChurch = adapt f
-  where
-    f :: ChurchListFunc
-    f = search "forall a. (forall r. (a -> r -> r) -> r -> r) -> (forall r. (a -> r -> r) -> r -> r)" $
-               \(f :: ChurchListFunc) ->
-                  let f' = adapt f in
-                  and [
-                      f' [] == ([] :: [()]),
-                      f' [1,2,3] == [1,2,3,3,2,1]
-                  ]
-    adapt :: ChurchListFunc -> [a] -> [a]
-    adapt f x = fromChurchList (getChurchListFunc f (toChurchList x))
+nil :: List a
+nil = List []
 
 
-sumSquaresChurch :: [Integer] -> Integer
-sumSquaresChurch = adapt f
-  where
-    f = search "(forall r. ((forall z. (z -> z) -> z -> z) -> r -> r) -> r -> r) -> (forall z. (z -> z) -> z -> z)" $
-              \(f :: ChurchList Church -> Church) ->
-                 let f' = adapt f in
-                 and [
-                    f' [] == 0,
-                    f' [1] == 1,
-                    f' [1,2] == 5,
-                    f' [1,2,4] == 21
-                  ]
-    adapt f x = fromChurch (f (toChurchList (map toChurch x)))
+deduce "snoc" [t| forall a. List a -> a -> List a |]
+    ['foldList, 'cons, 'nil]
+    [| and [ snoc (List []) 1 == List [1]
+           , snoc (List [1]) 2 == List [1,2]
+           , snoc (List [1,2,3]) 4 == List [1,2,3,4]
+           ] |]
 
-main = print $ reverseChurch [1,2,3,4,5] -- [5,4,3,2,1]
+
+-- Deducing reverse is too hard by itself, but if we do snoc first it's easy.
+deduce "reverseChurch" [t| forall a. List a -> List a |]
+    ['foldList, 'cons, 'snoc, 'nil]
+    [| and [ reverseChurch (List []) == List ([] :: [()])
+           , reverseChurch (List [1,2,3]) == List [3,2,1]
+           ] |]
+
+main = print $ reverseChurch (List [1,2,3,4,5])
